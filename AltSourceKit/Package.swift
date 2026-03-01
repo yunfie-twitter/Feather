96dfdfca2bc313fec25b15a37ebe46a935af58be
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-03-01 21:38:51 +0900

    AltSourceKit: set macOS deployment target to 11.0 (SwiftUI Color/NSColor init)

--- AltSourceKit/Package.swift
+++ AltSourceKit/Package.swift
@@ -8,17 +8,15 @@ let package = Package(
 	platforms: [
 		.iOS(.v14),
 		.tvOS(.v14),
+		.macOS(.v11),
 		.custom("xros", versionString: "1.3")
 	],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AltSourceKit",
             targets: ["AltSourceKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AltSourceKit"),
 
